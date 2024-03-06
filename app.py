486676f76f2a1ae813dbd75683c64db13fb2616f
i316984 <daisy.mao@sap.com>
2024-03-06 15:48:06 +0800

    chore:init.

--- app.py
+++ app.py
@@ -1,8 +1,77 @@
-# streamlit_huggingface_app.py
 import streamlit as st
+from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
+from langchain_community.vectorstores import FAISS
+from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
+import os
 
-st.title("Hugging Face + Streamlit App")
+CHUNK_SIZE = 1000
 
-# Streamlit 输入框，接收用户输入的文本
-user_input = st.text_input("Enter text for generation:", "Once upon a time")
+@st.cache_data
+def load_data(vector_store_dir: str = "faiss_index"):
+    load_dotenv()
+    os.environ["OPENAI_API_TYPE"] = "azure"
+    os.environ["OPENAI_API_VERSION"] = "2023-05-15"
+    os.environ["AZURE_OPENAI_ENDPOINT"] = "https://pvg-azure-openai-uk-south.openai.azure.com"
 
+    client = AzureOpenAIEmbeddings(
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        api_key="8d1daadc333e42b18e26d861588cfd43",
+        api_version="2023-05-15"
+    )
+
+    db = FAISS.load_local(vector_store_dir, client)
+    llm = AzureChatOpenAI(model_name="gpt-35-turbo", temperature=0.5, api_key="8d1daadc333e42b18e26d861588cfd43")
+
+    print("Loading data...")
+
+    AMAZON_REVIEW_BOT = RetrievalQA.from_chain_type(llm,
+                                                    retriever=db.as_retriever(search_type="similarity_score_threshold",
+                                                                              search_kwargs={"score_threshold": 0.7}))
+    AMAZON_REVIEW_BOT.return_source_documents = True
+
+    return AMAZON_REVIEW_BOT
+
+
+def chat(message, history):
+    print(f"[message]{message}")
+    print(f"[history]{history}")
+
+    AMAZON_REVIEW_BOT = load_data()
+
+    ans = AMAZON_REVIEW_BOT.invoke({"query": message})
+    if ans["source_documents"]:
+        return ans["result"]
+    else:
+        return "I don't know."
+
+
+if __name__ == "__main__":
+
+    st.title('IRM Help Review')
+
+    prompt = st.chat_input("Enter your questions here")
+
+    if "user_prompt_history" not in st.session_state:
+        st.session_state["user_prompt_history"] = []
+    if "chat_answers_history" not in st.session_state:
+        st.session_state["chat_answers_history"] = []
+    if "chat_history" not in st.session_state:
+        st.session_state["chat_history"] = []
+
+    if prompt:
+        with st.spinner("Generating......"):
+            output = chat(prompt, st.session_state["chat_history"])
+
+            st.session_state["chat_answers_history"].append(output)
+            st.session_state["user_prompt_history"].append(prompt)
+            st.session_state["chat_history"].append((prompt, output))
+
+    # Displaying the chat history
+
+    if st.session_state["chat_answers_history"]:
+        for i, j in zip(st.session_state["chat_answers_history"], st.session_state["user_prompt_history"]):
+            message1 = st.chat_message("user")
+            message1.write(j)
+            message2 = st.chat_message("assistant")
+            message2.write(i)
---
+++ create_vector_db.py
@@ -0,0 +1,38 @@
+from langchain.vectorstores import FAISS
+from pypdf import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
+import os
+from dotenv import load_dotenv
+
+CHUNK_SIZE = 1000
+
+
+# CREATE A VECTOR DATABASE - FAISS
+def create_vector_db(pdf) -> FAISS:
+    load_dotenv()
+    os.environ["OPENAI_API_TYPE"] = "azure"
+    os.environ["OPENAI_API_VERSION"] = "2023-05-15"
+    os.environ["AZURE_OPENAI_ENDPOINT"] = "https://pvg-azure-openai-uk-south.openai.azure.com"
+
+    client = AzureOpenAIEmbeddings(
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        api_key="8d1daadc333e42b18e26d861588cfd43",
+        api_version="2023-05-15"
+    )
+
+    text = ""
+    pdf_reader = PdfReader(pdf)
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE,
+                                                   chunk_overlap=100)
+    texts = text_splitter.split_text(text)
+
+    vector_db = FAISS.from_texts(texts, client)  # create vector db for similarity search
+    vector_db.save_local("faiss_index")  # save the vector db to avoid repeated calls to it
+    return vector_db
+
+directory_name = "IRM Help.pdf"
+create_vector_db(directory_name)
